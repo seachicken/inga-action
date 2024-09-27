5317917b26be547ec7b88f5d303b0a112979204f
seito.tanaka <seito.tanaka.dev@gmail.com>
2024-09-28 05:26:54 +0900

    test

--- java/entrypoint.sh
+++ java/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 git config --global --add safe.directory $PWD
-echo "$(cd $2 && git diff $1 --unified=0)"
-echo "$(cd $2 && git diff $1 --unified=0)" | inga --diff  --root-path $2 --exclude "$3" --diff -
+echo "$(cd $2 && git diff $1 --unified=0)" | inga --root-path $2 --exclude "$3" --diff -
