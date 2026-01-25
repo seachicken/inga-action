185d3e272d5267a379f33db8c8058f8609fa4b0d
seito.tanaka <seito.tanaka.dev@gmail.com>
2026-01-25 20:25:41 +0900

    test

---
+++ report/rollup.config.js
@@ -0,0 +1,12 @@
+import commonjs from "@rollup/plugin-commonjs";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+
+export default {
+  input: "src/index.js",
+  output: {
+    esModule: true,
+    file: "dist/index.js",
+    format: "es",
+  },
+  plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+};
